448b3aadd79c919f1934db7ac35a99f8e3ef5a32
xkid1 <elmer.alluad@gmail.com>
2023-04-02 18:14:13 +0800

    sh

--- sh/promp.sh
+++ sh/promp.sh
@@ -1,10 +1,1 @@
 #!/bin/sh
-# This is a comment
-
-# defining a variable
-echo "What is your name?"
-# reading input
-read NAME
-# defining a variable
-GREETINGS="Hello! How are you"
-echo $NAME $GREETINGS 
